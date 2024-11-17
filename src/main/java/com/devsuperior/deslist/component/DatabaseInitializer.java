aad56e1c8d6e1eff9ff364807f456c057de1171e
MarceloSilvaRosa <marcelosrosa83@gmail.com>
2024-11-17 18:39:17 -0300

    Game repository, service, controller

--- src/main/java/com/devsuperior/deslist/component/DatabaseInitializer.java
+++ src/main/java/com/devsuperior/deslist/component/DatabaseInitializer.java
@@ -1,4 +1,4 @@
-package com.devsuperior.deslist;
+package com.devsuperior.deslist.component;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
