9e47b726392b5caae84009755df622f414896f5b
MarceloSilvaRosa <marcelosrosa83@gmail.com>
2024-11-16 22:55:57 -0300

    Properties, Game e ORM

--- src/main/java/com/devsuperior/deslist/DeslistApplication.java
+++ src/main/java/com/devsuperior/deslist/DeslistApplication.java
@@ -2,12 +2,14 @@ package com.devsuperior.deslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"entities"})
 public class DeslistApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeslistApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DeslistApplication.class, args);
+    }
 
 }
